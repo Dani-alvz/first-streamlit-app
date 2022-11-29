4a75e37d9892f7d13c064e62a07e4c1981beec8a
Dani Álvarez <119429558+Dani-alvz@users.noreply.github.com>
2022-11-29 16:04:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My Parents New Helathy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text(' Omega 3 & Blueberry Oatmeal
-               Kale, Spinach & Rocket Smoothie
+streamlit.text(' Omega 3 & Blueberry Oatmeal /n
+               Kale, Spinach & Rocket Smoothie /n
                Hard-Boiled Free-Range Egg')
